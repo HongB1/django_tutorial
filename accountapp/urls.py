f54e2d2dc396a42f3206057f826aa58bba81c8f5
HongBi Kim <hongbikim@HongBiui-MacBookPro.local>
2023-05-07 18:36:00 +0900

    django course 21 - 'Implemenatation up via CreateView'

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreativeView
 
 app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    path('create/', AccountCreativeView.as_view(), name='create'), # 클래스형뷰는 as_view()를
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -21,3 +24,30 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
+
+class AccountCreativeView(CreateView):
+    # 네 가지 파라미터를 넘겨줬음
+    model = User #장고에서 기본으로 제공해주는 것
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') # reverse? reverse_lazy?
+    template_name = 'accountapp/create.html'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
