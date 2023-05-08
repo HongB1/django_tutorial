e78c1d965355f738cf75dfca1817a4f9f7aa136c
HongBi Kim <hongbikim@HongBiui-MacBookPro.local>
2023-05-08 14:42:46 +0900

    django course 22 - 'LoginView&LogoutView 구현' commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from accountapp.views import hello_world, AccountCreativeView
@@ -7,5 +8,8 @@ app_name = 'accountapp'
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
-    path('create/', AccountCreativeView.as_view(), name='create'), # 클래스형뷰는 as_view()를
+    path('create/', AccountCreativeView.as_view(), name='create'),  # 클래스형뷰는 as_view()를
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+
 ]
