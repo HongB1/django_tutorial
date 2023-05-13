836f1237977cb448b4cf8d96a036797a1725f79f
d <>
2023-05-13 18:24:41 +0900

    django course 25 commit

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import hello_world, AccountCreativeView, AccountDetailView
+from accountapp.views import hello_world, AccountCreativeView, AccountDetailView, AccountUpdateView
 
 app_name = 'accountapp'
 
@@ -11,5 +11,6 @@ urlpatterns = [
     path('create/', AccountCreativeView.as_view(), name='create'),  # 클래스형뷰는 as_view()를
     path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail')
+    path('detail/<int:pk>', AccountDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', AccountUpdateView.as_view(), name='update')
 ]
