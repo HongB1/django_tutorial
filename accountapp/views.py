3c4c0eeb0437020e6f172dc3a93be5746bf7121c
d <>
2023-05-13 18:45:12 +0900

    Bug Fix

--- accountapp/views.py
+++ accountapp/views.py
@@ -43,11 +43,13 @@ class AccountDetailView(DetailView):
 class AccountUpdateView(UpdateView):
     # 네 가지 파라미터를 넘겨줬음
     model = User  # 장고에서 기본으로 제공해주는 것
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')  # reverse? reverse_lazy?
     template_name = 'accountapp/update.html'
 
 class AccountDeleteView(DeleteView):
     model = User
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
